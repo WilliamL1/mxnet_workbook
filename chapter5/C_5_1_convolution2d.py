9732628d945dda2793677fb6046263125f3d01f9
李力 <lili@fcbox.com>
2019-07-30 10:34:06 +0800

    [2019-7-30]

---
+++ chapter5/C_5_1_convolution2d.py
@@ -0,0 +1,18 @@
+from mxnet import nd
+
+
+def corr2d(X, K):
+    h, w = K.shape
+    Y = nd.zeros((X.shape[0] - h + 1, X.shape[1] - w + 1))
+    for i in range(Y.shape[0]):
+        for j in range(Y.shape[1]):
+            Y[i,j] = (X[i:i + h, j:j + w] * K).sum()
+    return Y
+
+
+X = nd.array([[0, 1, 2], [3, 4, 5], [6, 7, 8]])
+K = nd.array([[0, 1], [2, 3]])
+
+Y = corr2d(X, K)
+
+print(Y)
